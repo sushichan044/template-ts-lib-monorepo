30a2fdafdf38ed8b09611ebd4d0f9751f85d790c
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-10-30 17:02:09 +0900

    scaffole

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     projects: ["packages/*"],
+    passWithNoTests: true,
+    typecheck: {
+      enabled: true,
+    },
   },
 });
